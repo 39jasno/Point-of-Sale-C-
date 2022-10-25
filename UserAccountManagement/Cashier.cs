83b02db90adde0aa6a504660b3101b19fe365f95
jasno10 <jasonmiran09@gmail.com>
2022-10-25 14:58:57 +0800

    Cashier menu, not done yet need return module

--- UserAccountManagement/Cashier.cs
+++ UserAccountManagement/Cashier.cs
@@ -28,14 +28,19 @@ namespace Group11_Machine_Problem
             {
                 if (check.IsAccountActive(username))
                 {
+                    OrderManage initOrder = new OrderManage();
+
                     Console.WriteLine("Cashier Menu");
-                    Console.WriteLine("\nPlease select an option from the menu:\n 1 - Manage your account \n 2 - {option 2}  \n 3 - Log out ");
+                    Console.WriteLine("\nPlease select an option from the menu:" +
+                        "\n 1 - Order Management" +
+                        "\n 2 - Returns Management" +
+                        "\n 3 - Log out ");
                     Console.Write("\nEnter choice: ");
                     choice = Console.ReadLine();
 
                     switch (choice)
                     {
-                        case "1": Console.Clear(); ManageUserAccount("Cashier User Management Menu\n"); break;
+                        case "1": Console.Clear(); initOrder.Main() ; break;
                         case "2": break;
                         case "3": Console.WriteLine("Logging out..."); Thread.Sleep(800); Console.Clear(); break;
                         default: Console.WriteLine("Invalid Input: Select a choice from the options!"); Thread.Sleep(800); Console.Clear(); break;
