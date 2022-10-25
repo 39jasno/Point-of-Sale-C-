c197af806fe096650432d4f947be82a9c62cc947
JustinMoscoso <116162941+JustinMoscoso@users.noreply.github.com>
2022-10-26 01:06:32 +0800

    Update OrderManagement.cs

--- OrdersManagementModule/OrderManagement.cs
+++ OrdersManagementModule/OrderManagement.cs
@@ -12,97 +12,85 @@ namespace Group11_Machine_Problem
         public void Main()
         {
 
-            Console.WriteLine("Choose product of your choice");
-            Console.WriteLine("1. Keyboard");
-            Console.WriteLine("2. Monitor");
-            Console.WriteLine("3. Mouse");
-            Console.WriteLine("4. Speaker");
-            Console.WriteLine("5. Headphones");
-            Console.WriteLine("6. Continue shopping");
-            int choice = Convert.ToInt32(Console.ReadLine());
-            int paymentOption;
+           int menu;
 
-            if (choice == 1)
+            Console.WriteLine("----Main Menu----");
+            Console.WriteLine("1. Create Order");
+            Console.WriteLine("2. Return Order");
+            menu = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            if (menu == 1)
             {
-                Keyboard keyboard = new Keyboard();
-            }
-
-            else if (choice == 2)
-            {
-                Monitor monitor = new Monitor();
-            }
-
-            else if (choice == 3)
-            {
-                Mouse mouse = new Mouse();
-            }
-
-            else if (choice == 4)
-            {
-                Speaker speaker = new Speaker();
-            }
-
-            else if (choice == 5)
-            {
-                Headphone headphone = new Headphone();
-            }
-
-            else
-            {
-                MessageBox.Show("Invalid");
-            }
-
-            Console.WriteLine("1. Credit Card");
-            Console.WriteLine("2. Cash on Delivery");
-            Console.WriteLine("Enter your Payment option");
-            paymentOption = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Choose product of your choice");
+                Console.WriteLine("1. Keyboard");
+                Console.WriteLine("2. Monitor");
+                Console.WriteLine("3. Mouse");
+                Console.WriteLine("4. Speaker");
+                Console.WriteLine("5. Headphones");
+                Console.WriteLine("6. Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
 
 
-            if (paymentOption == 1)
-            {
-                string num;
-                Console.WriteLine("Enter your 4 digit number");
-                num = Console.ReadLine();
-                int length = num.Length;
 
-                if (length == 4)
+
+
+                if (choice == 1)
                 {
-                    Console.WriteLine("Thank you for shopping!");
+                    Console.Clear();
+                    Keyboard keyboard = new Keyboard();
+                    StreamWriter moscosoWriter = new StreamWriter("KeyboardProducts");
+                    moscosoWriter.WriteLine(System.DateTime.Now.ToString());
+                    moscosoWriter.Close();
                 }
+
+
+
+                else if (choice == 2)
+                {
+                    Console.Clear();
+                    Monitor monitor = new Monitor();
+                    StreamWriter moscosoWriter = new StreamWriter("KeyboardProducts");
+                    moscosoWriter.WriteLine(System.DateTime.Now.ToString());
+                    moscosoWriter.Close();
+
+                }
+
+                else if (choice == 3)
+                {
+                    Console.Clear();
+                    Mouse mouse = new Mouse();
+                    StreamWriter moscosoWriter = new StreamWriter("KeyboardProducts");
+                    moscosoWriter.WriteLine(System.DateTime.Now.ToString());
+                    moscosoWriter.Close();
+                }
+
+                else if (choice == 4)
+                {
+                    Console.Clear();
+                    Speaker speaker = new Speaker();
+                    StreamWriter moscosoWriter = new StreamWriter("KeyboardProducts");
+                    moscosoWriter.WriteLine(System.DateTime.Now.ToString());
+                    moscosoWriter.Close();
+                }
+
+                else if (choice == 5)
+                {
+                    Console.Clear();
+                    Headphone headphone = new Headphone();
+                    float payment = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Thank you for shopping");
+                    DateTime dt = new DateTime();
+                    Console.WriteLine(dt);
+                }
+
                 else
                 {
                     MessageBox.Show("Invalid");
+
                 }
+
+                Console.ReadKey();
             }
-
-
-            else if (paymentOption == 2)
-            {
-                string name;
-                string address;
-                string contactNumber;
-                Console.WriteLine("Enter your name");
-                name = Console.ReadLine();
-                Console.WriteLine("Enter your address");
-                address = Console.ReadLine();
-                Console.WriteLine("Enter your 11 digit Phone Number");
-                contactNumber = Console.ReadLine();
-                int length = contactNumber.Length;
-                if (length == 11)
-                {
-                    Console.WriteLine("Thank you for shopping");
-                }
-                else
-                {
-                    MessageBox.Show("Invalid Number");
-                }
-
-
-            }
-
-
-
-            Console.ReadKey();
         }
     }
 }
