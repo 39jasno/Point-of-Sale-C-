768b87200de8e2e0b25bd0d416eb0c48c0570919
jasno10 <jasonmiran09@gmail.com>
2022-10-26 09:21:09 +0800

    we

--- OrdersManagementModule/OrderManagement.cs
+++ OrdersManagementModule/OrderManagement.cs
@@ -22,28 +22,28 @@ namespace Group11_Machine_Problem
             Console.Clear();
             if (menu == 1)
             {
-                Console.WriteLine("Choose product of your choice");
-                Console.WriteLine("1. Keyboard");
-                Console.WriteLine("2. Monitor");
-                Console.WriteLine("3. Mouse");
-                Console.WriteLine("4. Speaker");
-                Console.WriteLine("5. Headphones");
-                Console.WriteLine("6. Exit");
-                int choice = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Choose product of your choice" +
+                    "\n1. Keyboard"+
+                    "\n2. Monitor"+
+                    "\n3. Mouse"+
+                    "\n4. Speaker"+
+                    "\n5. Headphones"+
+                    "\n6. Exit"+
+                    "\n\nInput coice: ");
+
+                string choice = Console.ReadLine();
 
 
-                if (choice == 1)
+                if (choice == "1")
                 {
                     Console.WriteLine();
                     Keyboard keyboard = new Keyboard();
-                    StreamWriter moscosoWriter = new StreamWriter("Products.txt");
+                    StreamWriter writer = new StreamWriter("Products.txt");
                     DateTime dt = new DateTime();
                     Console.WriteLine(dt);
                 }
 
-
-
-                else if (choice == 2)
+                else if (choice == "2")
                 {
                     Console.WriteLine();
                     Monitor monitor = new Monitor();
@@ -53,7 +53,7 @@ namespace Group11_Machine_Problem
 
                 }
 
-                else if (choice == 3)
+                else if (choice == "3")
                 {
                     Console.WriteLine();
                     Mouse mouse = new Mouse();
@@ -62,7 +62,7 @@ namespace Group11_Machine_Problem
                     Console.WriteLine(dt);
                 }
 
-                else if (choice == 4)
+                else if (choice == "5")
                 {
                     Console.WriteLine();
                     Speaker speaker = new Speaker();
@@ -71,7 +71,7 @@ namespace Group11_Machine_Problem
                     Console.WriteLine(dt);
                 }
 
-                else if (choice == 5)
+                else if (choice == "6")
                 {
                     Console.WriteLine();
                     Headphone headphone = new Headphone();
@@ -91,9 +91,12 @@ namespace Group11_Machine_Problem
             }
             else
             {
-                StreamWriter moscosoWriter = new StreamWriter("Products.txt");
-                moscosoWriter.WriteLine(System.DateTime.Now.ToString());
-                moscosoWriter.Close();
+                using (StreamWriter writer = new StreamWriter("Products.txt"))
+                {
+                    writer.WriteLine(System.DateTime.Now.ToString());
+                    writer.Close();
+                }
+                
 
             }
             Console.ReadKey();
