031e4b00568f49ed43ac4f9411e0b3e5d8f30a24
jasno10 <jasonmiran09@gmail.com>
2022-10-28 23:24:37 +0800

    we

---
+++ ReportsManagementModule/InitializeSales.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Group11_Machine_Problem.ReportsManagementModule
+{
+    class InitializeSales
+    {
+        public void Initialize()
+        {
+            string path = "Sales.txt";
+            string[] dailySales;
+            string[] productSales;
+            string[] itemReturn;
+            foreach (var line in File.ReadLines(path))
+            {
+                string[] array = line.Split('|');
+
+            }
+        }
+        
+
+    }
+}
